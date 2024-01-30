e30ac5ab1e3c9101510e7c2a57cf6dc385520db1
ShyneADL <benex2002@gmail.com>
2024-01-30 06:27:13 +0100

    updated the receipt pages

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"I/InGzhd7O8ZFzbQb9sr1jNwqzKwXMXRLlQGciSJ47Y=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UQkIaN+FGBD+VwOY82RsKoNy4LduKqPgwOutwyPOA1E=\"\n}"
